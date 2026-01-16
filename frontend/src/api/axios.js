6b398a343a712eae28a3c42bbe6ee9de80d3b887
cedrix21 <ravelocedrix@gmail.com>
2026-01-16 06:22:24 -0500

    Point frontend to live Railway API

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/api",
+  baseURL: onlineenrollmentims-production.up.railway.app/api,
   headers: {
         'Accept': 'application/json', 
         'Content-Type': 'application/json',
