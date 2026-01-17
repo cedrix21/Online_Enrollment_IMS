3af5c75fdfd2eeefc971857c017a503be0de369e
cedrix21 <ravelocedrix@gmail.com>
2026-01-17 04:46:34 -0500

    Updating the login.js

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,43 +8,7 @@ use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\SectionController;
 use App\Http\Controllers\ScheduleController;
 use App\Models\Subject;
-use App\Models\Enrollment;
-use App\Models\Section;
-use Barryvdh\DomPDF\Facade\Pdf;
-use Sabberworm\CSS\Property\Import;
 
-
-
-
-
-
-Route::get('/test-pdf', function () {
-    // 1. Grab a dummy enrollment and section from your DB
-    $enrollment = Enrollment::first(); 
-    $section = Section::with(['schedules.subject', 'schedules.time_slot', 'schedules.room', 'advisor'])->first();
-
-    if (!$enrollment || !$section) {
-        return "Please ensure you have at least one enrollment and one section in the database to test.";
-    }
-    // --- ADD THIS LOGO LOGIC HERE ---
-    $logoPath = public_path('assets/sics-logo.png');
-    $logoBase64 = '';
-    if (file_exists($logoPath)) {
-        $logoData = base64_encode(file_get_contents($logoPath));
-        $logoBase64 = 'data:image/png;base64,' . $logoData;
-    }
-
-    // 2. Render the view
-    $pdf = Pdf::loadView('pdf.loadslip', [
-        'enrollment' => $enrollment,
-        'section'    => $section,
-        'studentId'  => 'SICS-2026-0013',
-        'logo'       => $logoBase64
-    ]);
-
-    // 3. Stream it to the browser (instead of downloading)
-    return $pdf->stream('test-loadslip.pdf');
-});
 
 
 
@@ -63,7 +27,7 @@ Route::post('/login', [AuthController::class, 'login']);
 */
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user-test', function (Request $request) {
+    Route::get('/user', function (Request $request) {
         return response()->json($request->user());
     });
 
