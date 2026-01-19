4eec0926360bc10fbf57649efd01041e0a4a3691
cedrix21 <ravelocedrix@gmail.com>
2026-01-19 04:48:20 -0500

    changing sendinblue to BREVO_API_KEY

--- backend/config/services.php
+++ backend/config/services.php
@@ -34,8 +34,8 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
-    'sendinblue' => [
-    'key' => env('BREVO_KEY'),
-    ],
+    'brevo' => [
+    'key' => env('BREVO_API_KEY'),
+],
 
 ];
