07b6ac7d9de62eaea94b68024514e21d6bd9db22
cedrix21 <ravelocedrix@gmail.com>
2026-01-19 16:38:28 +0800

    sendidblue

--- backend/config/mail.php
+++ backend/config/mail.php
@@ -49,6 +49,10 @@ return [
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
+        'sendinblue' => [
+            'transport' => 'sendinblue',
+        ],
+
         'ses' => [
             'transport' => 'ses',
         ],
