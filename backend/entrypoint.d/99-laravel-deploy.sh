8ba93d9802acb5696035099fbde67e6a33bb6925
cedrix21 <ravelocedrix@gmail.com>
2026-04-24 14:33:59 +0800

    fix: use entrypoint.d for serversideup php image

---
+++ backend/entrypoint.d/99-laravel-deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+echo "=== [1/3] Caching config & routes... ==="
+php artisan config:clear
+php artisan config:cache
+php artisan route:cache
+
+echo "=== [2/3] Running migrations... ==="
+php artisan migrate --force
+
+echo "=== [3/3] Done! Starting server... ==="
